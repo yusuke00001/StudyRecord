cd14019d9b152b26d7f1dd133c5f3cd84b8358da
ゆうすけ <yuusuke@naitouyuusukenoMacBook-Air.local>
2026-02-04 20:40:31 +0900

    Initial commit

---
+++ src/main/java/com/example/studyrecord/StudyRecordApplication.java
@@ -0,0 +1,13 @@
+package com.example.studyrecord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyRecordApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyRecordApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/studyrecord/StudyRecordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studyrecord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyRecordApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
